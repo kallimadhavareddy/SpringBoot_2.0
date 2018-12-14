560839ef50fa1b8023df9180188ac626e0f12f9f
Madhava Reddy <kalli.madhavareddy@gmail.com>
2018-12-14 10:38:33 -0800

    digiatal-dairy initial spring.io download

---
+++ src/main/java/tk/madhavareddy/digital/dairy/digitaldairy/DigitalDairyApplication.java
@@ -0,0 +1,14 @@
+package tk.madhavareddy.digital.dairy.digitaldairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalDairyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalDairyApplication.class, args);
+	}
+
+}
+
