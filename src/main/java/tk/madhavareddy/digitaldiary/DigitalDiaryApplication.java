cceac28e1b7ff161968b8e779d8360d52c70e3b0
Madhava Reddy <kalli.madhavareddy@gmail.com>
2018-12-20 12:18:33 -0800

    H2 data base Spring Boot example with 1.model mappers as @Component 2.@GetMapping 3.@PostMapping 4.@RequestBody 5.@Valid 6.DAO Layer with JPA repository. 7.SSL Certificate Changes 8.Swagger Implementation

--- src/main/java/tk/madhavareddy/digitaldiary/DigitalDiaryApplication.java
+++ src/main/java/tk/madhavareddy/digitaldiary/DigitalDiaryApplication.java
@@ -4,8 +4,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DigitalDiaryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalDiaryApplication.class, args);
@@ -13,6 +19,15 @@ public class DigitalDiaryApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 }
 
