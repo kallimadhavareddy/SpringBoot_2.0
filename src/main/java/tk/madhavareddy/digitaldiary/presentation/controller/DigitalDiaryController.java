1aed98ef454de2044d63945c2dc5c7e10cd25dc5
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-07-25 00:31:39 +0200

    Added example folder reading

--- src/main/java/tk/madhavareddy/digitaldiary/presentation/controller/DigitalDiaryController.java
+++ src/main/java/tk/madhavareddy/digitaldiary/presentation/controller/DigitalDiaryController.java
@@ -2,21 +2,25 @@ package tk.madhavareddy.digitaldiary.presentation.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import tk.madhavareddy.digitaldiary.persistence.entity.Event;
 import tk.madhavareddy.digitaldiary.presentation.data.Diary;
 import tk.madhavareddy.digitaldiary.presentation.util.ObjectMapperUtils;
 import tk.madhavareddy.digitaldiary.process.service.DigitalDiaryService;
 import javax.validation.Valid;
+import java.io.File;
 import java.util.List;
-import java.util.Set;
 
 @RestController
 @Slf4j
 public class DigitalDiaryController {
 
 	private final DigitalDiaryService digitalDiaryService;
+
+	@Value("${test.path}")
+	String filePath;
+	@Value("${test.path2}")
+	String filePath2;
 
 
 	@Autowired
@@ -47,6 +51,30 @@ public class DigitalDiaryController {
 	@GetMapping("diaries/{diaryId}/diary")
 	public Diary getDiary(@PathVariable Integer diaryId){
 		return digitalDiaryService.getDiary(diaryId);
+	}
+
+	@GetMapping("/test/file")
+	public String fileTest(){
+
+		File file = new File(filePath+"/First.txt");
+		if(file.exists()) {
+			return "Yes file exists in the path "+file.getAbsolutePath();
+		}
+
+		return "not exists";
+	}
+
+	@GetMapping("/test/file2")
+	public String fileTest2(){
+
+		File file = new File(filePath2+ "/Second.txt");
+		if(file.exists()) {
+			return "Yes file exists in the path "+file.getAbsolutePath();
+		}
+
+
+
+		return file.getAbsolutePath();
 	}
 
 }
