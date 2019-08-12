70976fc4c5fc856f94113fba751bfe4099f8aac9
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-08-12 21:33:34 +0200

    Spring Boot sample project

--- src/main/java/tk/madhavareddy/digitaldiary/process/controller/DigitalDiaryController.java
+++ src/main/java/tk/madhavareddy/digitaldiary/process/controller/DigitalDiaryController.java
@@ -5,22 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import tk.madhavareddy.digitaldiary.presentation.data.Diary;
-import tk.madhavareddy.digitaldiary.process.service.DigitalDiaryService;
 
 @RestController
 public class DigitalDiaryController {
-	
-	private DigitalDiaryService digitalDiaryService;
-	
-	@Autowired
-	public void DigitalDiaryService(DigitalDiaryService digitalDiaryService){
-	this.digitalDiaryService = digitalDiaryService;	
-	}	
-
 	@GetMapping("/diaries/{diaryId}/diary")	
-	public Diary getContent(@PathVariable String diaryId) {
-
-		return digitalDiaryService.getDiary(diaryId);
+	public String getDiary() {
+		return "Spring boot Sample!, Say everyone Hello to digital Diary project";
 	}	
 }
