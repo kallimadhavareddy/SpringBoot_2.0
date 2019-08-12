b780b8aa29b71b05619b4c3177f0a9052216b4ac
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-08-12 21:46:26 +0200

    Spring Boot sample project

--- src/main/java/tk/madhavareddy/digitaldiary/process/controller/DigitalDiaryController.java
+++ src/main/java/tk/madhavareddy/digitaldiary/process/controller/DigitalDiaryController.java
@@ -1,10 +1,7 @@
 package tk.madhavareddy.digitaldiary.process.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class DigitalDiaryController {
