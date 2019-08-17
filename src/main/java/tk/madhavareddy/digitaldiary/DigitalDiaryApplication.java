6204988d0955aabe08d14c4eb40d6ffcf35deaeb
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-08-17 12:38:11 +0200

    one to one relation ship with exception handling and model mappers

--- src/main/java/tk/madhavareddy/digitaldiary/DigitalDiaryApplication.java
+++ src/main/java/tk/madhavareddy/digitaldiary/DigitalDiaryApplication.java
@@ -2,7 +2,6 @@ package tk.madhavareddy.digitaldiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class DigitalDiaryApplication {
