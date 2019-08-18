f67fbdf947dcd9ce1578914ae3dc85191c2a77b6
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-08-18 17:59:51 +0200

    Oauth2.0 Implementation

--- src/main/java/tk/madhavareddy/digitaldiary/persistence/dao/UserDao.java
+++ src/main/java/tk/madhavareddy/digitaldiary/persistence/dao/UserDao.java
@@ -1,21 +1,8 @@
 package tk.madhavareddy.digitaldiary.persistence.dao;
 
-
-
 import tk.madhavareddy.digitaldiary.persistence.entity.User;
 
 import java.util.List;
-
-
-/**
- * Created by IntelliJ IDEA.
- * Project : oauth2.0
- * User: madhavareddy
- * Email: kalli.madhavareddy@gmail.com
- * Date: 05/08/2019
- * Time: 16.06
- * To change this template use File | Settings | File Templates.
- */
 
 public interface UserDao {
     User save(User user);
--- src/main/java/tk/madhavareddy/digitaldiary/persistence/entity/User.java
+++ src/main/java/tk/madhavareddy/digitaldiary/persistence/entity/User.java
@@ -9,37 +9,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-/**
- * Created by IntelliJ IDEA.
- * Project : spring-boot-security-oauth2-example
- * User: madhavareddy
- * Email: kalli.madhavareddy@gmail.com
- * Date: 05/08/2019
- * Time: 16.06
- * To change this template use File | Settings | File Templates.
- */
-
 @Data
 @Entity
 @Table(name = "tbl_user")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     @Column
     private String username;
-
     @Column
     @JsonIgnore
     private String password;
-
     @Column
     private long salary;
-
     @Column
     private int age;
-
 }
