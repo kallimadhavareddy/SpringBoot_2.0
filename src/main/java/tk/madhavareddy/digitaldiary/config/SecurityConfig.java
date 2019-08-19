19ddb4e161f12c0c0826417936c2bf42c1e90cca
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-08-20 01:17:30 +0200

    Oauth2.0 Implementation

--- src/main/java/tk/madhavareddy/digitaldiary/config/SecurityConfig.java
+++ src/main/java/tk/madhavareddy/digitaldiary/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package tk.madhavareddy.digitaldiary.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import javax.annotation.Resource;
 
 @Configuration
+
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource(name = "userService")
     private UserDetailsService userDetailsService;
@@ -26,9 +29,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().fullyAuthenticated()
+
+        http.httpBasic()
                 .and()
-                .httpBasic();
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/diaries/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/diaries").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/diaries/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/diaries/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/diaries/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,6 @@ INSERT INTO tbl_user (id, username, password, salary, age) VALUES (2, 'madhava',
 INSERT INTO tbl_user (id, username, password, salary, age) VALUES (3, 'reddy', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 45);
 INSERT INTO tbl_user (id, username, password, salary, age) VALUES (4, 'aakash', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 4234, 40);
 
-
 -- hendi / password
 -- sasuke / password
 -- naruto / password
