b3ece7849d063ed38cc1eef4c775bfadb0ba0000
Madhava Reddy <kalli.madhavareddy@gmail.com>
2019-08-20 21:43:22 +0200

    Documentation for 1.Basic Auth 2.OAuth 2.0 3.JPA composite key 4.JPA One to One

--- src/main/java/tk/madhavareddy/digitaldiary/persistence/entity/Diary.java
+++ src/main/java/tk/madhavareddy/digitaldiary/persistence/entity/Diary.java
@@ -8,9 +8,10 @@ import tk.madhavareddy.digitaldiary.persistence.model.Status;
 
 
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDate;
 
@@ -20,8 +21,9 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 public class Diary {
-    @EmbeddedId
-    CompositeIdentity compositeIdentity;
+    @Id
+    @GeneratedValue
+    int id;
     @Column(name="CONTENT")
     String content;
     @Column(name="CONTENT_DATE")
--- src/main/java/tk/madhavareddy/digitaldiary/persistence/repository/DigitalDiaryRepository.java
+++ src/main/java/tk/madhavareddy/digitaldiary/persistence/repository/DigitalDiaryRepository.java
@@ -2,9 +2,8 @@ package tk.madhavareddy.digitaldiary.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import tk.madhavareddy.digitaldiary.persistence.entity.CompositeIdentity;
 import tk.madhavareddy.digitaldiary.persistence.entity.Diary;
 
 @Repository
-public interface DigitalDiaryRepository extends JpaRepository<Diary, CompositeIdentity> {
+public interface DigitalDiaryRepository extends JpaRepository<Diary, Integer> {
 }
