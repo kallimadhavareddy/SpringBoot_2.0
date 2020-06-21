c8bc8b0330836853587086008fc04ef67d09ac38
Madhava Reddy <kalli.madhavareddy@gmail.com>
2020-06-21 21:00:11 +0200

    Global Exception handling changes

--- src/main/java/tk/madhavareddy/digitaldiary/presentation/exception/ExceptionHandlerControllerAdvise.java
+++ src/main/java/tk/madhavareddy/digitaldiary/presentation/exception/ExceptionHandlerControllerAdvise.java
@@ -3,6 +3,7 @@ package tk.madhavareddy.digitaldiary.presentation.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,15 @@ public class ExceptionHandlerControllerAdvise {
     public ResponseEntity<ErrorResponse> handleException(ResourceNotFoundException ex,final HttpServletRequest request) {
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setErrorResponse(ex.getMessage());
+        errorResponse.setErrorStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorResponse.setRequestURI(request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotfound(MethodArgumentNotValidException maex, final HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setErrorResponse(maex.getMessage());
         errorResponse.setErrorStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         errorResponse.setRequestURI(request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
